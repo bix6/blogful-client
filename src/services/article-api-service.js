1f02e14c7ccfa7d2daa872cd732fe572ec67acde
bix <bixmail6@gmail.com>
2020-09-14 16:16:36 -0700

    finish 18.5 JWT secure login

--- src/services/article-api-service.js
+++ src/services/article-api-service.js
@@ -11,7 +11,7 @@ const ArticleApiService = {
   },
   getArticle(articleId) {
     return fetch(`${config.API_ENDPOINT}/articles/${articleId}`, {
-      headers: { authorization: `basic ${TokenService.getAuthToken()}` },
+      headers: { authorization: `bearer ${TokenService.getAuthToken()}` },
     }).then((res) =>
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
@@ -19,7 +19,7 @@ const ArticleApiService = {
   getArticleComments(articleId) {
     return fetch(`${config.API_ENDPOINT}/articles/${articleId}/comments`, {
       headers: {
-        authorization: `basic ${TokenService.getAuthToken()}`,
+        authorization: `bearer ${TokenService.getAuthToken()}`,
       },
     }).then((res) =>
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
@@ -30,7 +30,7 @@ const ArticleApiService = {
       method: "POST",
       headers: {
         "content-type": "application/json",
-        authorization: `basic ${TokenService.getAuthToken()}`,
+        authorization: `bearer ${TokenService.getAuthToken()}`,
       },
       body: JSON.stringify({
         article_id: articleId,
